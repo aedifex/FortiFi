590c758912ad165e17b5eb72db4a44dfbf777dcf
jonathan.nguyen <jonathan.nguyen@berkeley.edu>
2025-02-19 20:36:03 -0500

    add counts to users table for network distribution

--- server/init.sql
+++ server/init.sql
@@ -9,6 +9,9 @@ Create Table Users (
     last_name varchar(64) NOT NULL,
     email varchar(64) NOT NULL,
     fcm_token varchar(255),
+    normal_count int NOT NULL DEFAULT 0,
+    anomalous_count int NOT NULL DEFAULT 0,
+    malicious_count int NOT NULL DEFAULT 0,
     password varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -37,7 +40,7 @@ CREATE TABLE NetworkEvents (
     details varchar(255) NOT NULL,
     ts DATETIME NOT NULL,
     expires DATETIME NOT NULL,
-    event_type varchar(255) NOT NULL, -- normal, anomalous, malicious
+    event_type varchar(255) NOT NULL, --  anomalous or malicious
     FOREIGN KEY (id) REFERENCES Users(id) ON DELETE CASCADE
 );
 CREATE INDEX NetworkEvents_Id_Index ON NetworkEvents(id ASC);
