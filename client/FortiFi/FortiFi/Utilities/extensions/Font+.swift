7244a311bd1b370d801d3efba30e21466964ddef
jonathan.nguyen <jonathan.nguyen@berkeley.edu>
2025-02-21 11:38:29 -0500

    add font extension

---
+++ client/FortiFi/FortiFi/Utilities/extensions/Font+.swift
@@ -0,0 +1,19 @@
+//
+//  Font+.swift
+//  FortiFi
+//
+//  Created by Jonathan Nguyen on 2/21/25.
+//
+
+import Foundation
+import SwiftUICore
+
+extension Font {
+    static let fortifiTitle: Font = .custom("Inter", size: 24).weight(.medium)
+    static let fortifiHeader: Font = .custom("Inter", size: 20).weight(.regular)
+    static let fortifiBody: Font = .custom("Inter", size: 16).weight(.regular)
+    static let fortifiLabel: Font = .custom("Inter", size: 14).weight(.regular)
+    static let fortifiButtons: Font = .custom("Inter", size: 14).weight(.medium)
+    static let fortifiNumbers: Font = .custom("Inter", size: 16).weight(.medium)
+}
+
